ec426b46b55112d5b5eb9ab99d8e78fd27569186
reocraft <reocraft.creeper@gmail.com>
2025-11-23 16:16:56 -0600

    Implementation working

--- src/main/java/edu/grinnell/csc207/compression/Grin.java
+++ src/main/java/edu/grinnell/csc207/compression/Grin.java
@@ -1,49 +1,150 @@
 package edu.grinnell.csc207.compression;
 
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * The driver for the Grin compression program.
  */
 public class Grin {
+
+    public static final int MAGIC_NUM = 0x736;
+
     /**
      * Decodes the .grin file denoted by infile and writes the output to the
      * .grin file denoted by outfile.
-     * @param infile the file to decode
+     * 
+     * @param infile  the file to decode
      * @param outfile the file to ouptut to
+     * @throws Exception
      */
-    public static void decode (String infile, String outfile) {
-        // TODO: fill me in!
+    public static void decode(String infile, String outfile) throws Exception {
+        BitInputStream input = null;
+        BitOutputStream output = null;
+        String inputFile = "files/" + infile;
+        String outputFile = "files/" + outfile;
+
+        try {
+            input = new BitInputStream(inputFile);
+            int magicCode = input.readBits(32);
+            if (magicCode != MAGIC_NUM) {
+                throw new Exception(infile + " is not a valid Grin file.");
+            }
+            HuffmanTree tree = new HuffmanTree(input);
+            output = new BitOutputStream(outputFile);
+            tree.decode(input, output);
+
+        } catch (Exception e) {
+            throw new Exception("We faced an exception in decoding.", e);
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+            if (output != null) {
+                output.close();
+            }
+        }
     }
 
     /**
      * Creates a mapping from 8-bit sequences to number-of-occurrences of
      * those sequences in the given file. To do this, read the file using a
      * BitInputStream, consuming 8 bits at a time.
+     * 
      * @param file the file to read
      * @return a freqency map for the given file
+     * @throws Exception
      */
-    public static Map<Short, Integer> createFrequencyMap (String file) {
-        // TODO: fill me in!
-        return null;
+    public static Map<Short, Integer> createFrequencyMap(String file) throws Exception {
+        Map<Short, Integer> freqMap = new HashMap<>();
+        BitInputStream input = null;
+        try {
+            input = new BitInputStream(file);
+            while (true) {
+                short bits = (short) input.readBits(8);
+                if (bits == -1) {
+                    break;
+                }
+                int count;
+                if (freqMap.containsKey(bits)) {
+                    count = freqMap.get(bits) + 1;
+                } else {
+                    count = 1;
+                }
+                freqMap.put(bits, count);
+            }
+
+        } catch (Exception e) {
+            throw new Exception("We faced an exception in creating the frequency map.", e);
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+        }
+        return freqMap;
     }
 
     /**
      * Encodes the given file denoted by infile and writes the output to the
      * .grin file denoted by outfile.
-     * @param infile the file to encode.
+     * 
+     * @param infile  the file to encode.
      * @param outfile the file to write the output to.
+     * @throws Exception
      */
-    public static void encode(String infile, String outfile) {
-        // TODO: fill me in!
+    public static void encode(String infile, String outfile) throws Exception {
+        BitInputStream input = null;
+        BitOutputStream output = null;
+        String inputFile = "files/" + infile;
+        String outputFile = "files/" + outfile;
+
+        try {
+            Map<Short, Integer> freqMap = createFrequencyMap(inputFile);
+            HuffmanTree tree = new HuffmanTree(freqMap);
+
+            output = new BitOutputStream(outputFile);
+            output.writeBits(MAGIC_NUM, 32);
+            tree.serialize(output);
+
+            input = new BitInputStream(inputFile);
+            tree.encode(input, output);
+
+        } catch (Exception e) {
+            throw new Exception("We faced an exception in encoding.", e);
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+            if (output != null) {
+                output.close();
+            }
+        }
     }
 
     /**
      * The entry point to the program.
+     * 
      * @param args the command-line arguments.
+     * @throws Exception
      */
-    public static void main(String[] args) {
-        // TODO: fill me in!
-        System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+    public static void main(String[] args) throws Exception {
+
+        if (args.length != 3) {
+            System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+            return;
+        }
+
+        String code = args[0];
+        String infile = args[1];
+        String outfile = args[2];
+
+        if (code.equals("encode")) {
+            encode(infile, outfile);
+        } else if (code.equals("decode")) {
+            decode(infile, outfile);
+        } else {
+            System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+        }
+
     }
 }
--- src/main/java/edu/grinnell/csc207/compression/HuffmanTree.java
+++ src/main/java/edu/grinnell/csc207/compression/HuffmanTree.java
@@ -1,55 +1,173 @@
 package edu.grinnell.csc207.compression;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
  * A HuffmanTree derives a space-efficient coding of a collection of byte
  * values.
  *
  * The huffman tree encodes values in the range 0--255 which would normally
- * take 8 bits.  However, we also need to encode a special EOF character to
- * denote the end of a .grin file.  Thus, we need 9 bits to store each
- * byte value.  This is fine for file writing (modulo the need to write in
+ * take 8 bits. However, we also need to encode a special EOF character to
+ * denote the end of a .grin file. Thus, we need 9 bits to store each
+ * byte value. This is fine for file writing (modulo the need to write in
  * byte chunks to the file), but Java does not have a 9-bit data type.
  * Instead, we use the next larger primitive integral type, short, to store
  * our byte values.
  */
 public class HuffmanTree {
 
+    private class Node implements Comparable<Node> {
+        short value;
+        int freq;
+        Node left;
+        Node right;
+
+        public Node(short value, int freq) {
+            this.value = value;
+            this.freq = freq;
+        }
+
+        public Node(Node left, Node right) {
+            this.freq = left.freq + right.freq;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int compareTo(Node other) {
+            return Integer.compare(this.freq, other.freq);
+        }
+
+        public boolean isLeaf() {
+            return (this instanceof Leaf);
+        }
+    }
+
+    private class Leaf extends Node {
+        public Leaf(short value, int freq) {
+            super(value, freq);
+        }
+    }
+
+    private class Branch extends Node {
+        public Branch(Node left, Node right) {
+            super(left, right);
+        }
+    }
+
+    private Node root;
+
     /**
      * Constructs a new HuffmanTree from a frequency map.
+     * 
      * @param freqs a map from 9-bit values to frequencies.
      */
-    public HuffmanTree (Map<Short, Integer> freqs) {
-        // TODO: fill me in!
+    public HuffmanTree(Map<Short, Integer> freqs) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        for (Map.Entry<Short, Integer> e : freqs.entrySet()) {
+            pq.add(new Leaf(e.getKey(), e.getValue()));
+        }
+
+        short EOF = 256;
+        pq.add(new Leaf(EOF, 1));
+
+        while (pq.size() > 1) {
+            Node x = pq.remove();
+            Node y = pq.remove();
+
+            pq.add(new Branch(x, y));
+        }
+
+        this.root = pq.remove();
     }
 
     /**
      * Constructs a new HuffmanTree from the given file.
+     * 
      * @param in the input file (as a BitInputStream)
      */
-    public HuffmanTree (BitInputStream in) {
-        // TODO: fill me in!
+    public HuffmanTree(BitInputStream in) {
+        this.root = readTree(in);
+    }
+
+    private Node readTree(BitInputStream in) {
+        int bit = in.readBit();
+        if (bit == 0) {
+            short value = (short) in.readBits(9);
+            return new Leaf(value, 0);
+        } else { // internal
+            Node left = readTree(in);
+            Node right = readTree(in);
+            return new Branch(left, right);
+        }
     }
 
     /**
      * Writes this HuffmanTree to the given file as a stream of bits in a
      * serialized format.
+     * 
      * @param out the output file as a BitOutputStream
      */
-    public void serialize (BitOutputStream out) {
-        // TODO: fill me in!
+    public void serialize(BitOutputStream out) {
+        serializeH(root, out);
     }
-   
+
+    public void serializeH(Node n, BitOutputStream out) {
+        if (n.isLeaf()) {
+            out.writeBit(0);
+            out.writeBits(n.value, 9);
+        } else {
+            out.writeBit(1);
+            serializeH(n.left, out);
+            serializeH(n.right, out);
+        }
+    }
+
     /**
      * Encodes the file given as a stream of bits into a compressed format
      * using this Huffman tree. The encoded values are written, bit-by-bit
      * to the given BitOuputStream.
-     * @param in the file to compress.
+     * 
+     * @param in  the file to compress.
      * @param out the file to write the compressed output to.
      */
-    public void encode (BitInputStream in, BitOutputStream out) {
-        // TODO: fill me in!
+    public void encode(BitInputStream in, BitOutputStream out) {
+        Map<Short, String> hm = new HashMap<>();
+        encodeH(root, "", hm);
+
+        while (true) {
+            short key = (short) in.readBits(8);
+            if (key == -1) {
+                break; // Hit end of file
+            }
+            String value = hm.get(key);
+            for (char c : value.toCharArray()) {
+                if (c == '0') {
+                    out.writeBit(0);
+                } else {
+                    out.writeBit(1);
+                }
+            }
+        }
+        String eofVal = hm.get((short) 256);
+        for (char c : eofVal.toCharArray()) {
+            if (c == '0') {
+                out.writeBit(0);
+            } else {
+                out.writeBit(1);
+            }
+        }
+    }
+
+    private void encodeH(Node n, String path, Map<Short, String> map) {
+        if (n.isLeaf()) {
+            map.put(n.value, path);
+            return;
+        }
+        encodeH(n.left, path + "0", map);
+        encodeH(n.right, path + "1", map);
     }
 
     /**
@@ -57,10 +175,29 @@ public class HuffmanTree {
      * bits into their uncompressed form, saving the results to the given
      * output stream. Note that the EOF character is not written to out
      * because it is not a valid 8-bit chunk (it is 9 bits).
-     * @param in the file to decompress.
+     * 
+     * @param in  the file to decompress.
      * @param out the file to write the decompressed output to.
      */
-    public void decode (BitInputStream in, BitOutputStream out) {
-        // TODO: fill me in!
+    public void decode(BitInputStream in, BitOutputStream out) {
+
+        Node cur = root;
+        while (true) {
+            int bit = in.readBit();
+
+            if (bit == 0) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+
+            if (cur.isLeaf()) {
+                if (cur.value == 256) {
+                    break; // We hit EOF;
+                }
+                out.writeBits(cur.value, 8);
+                cur = root;
+            }
+        }
     }
 }
